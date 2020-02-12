68e6ff4ac629b7b06be245cfdac2d9b7309ebb43
Student bei LernMoment <student@lernmoment.de>
2020-02-12 09:39:12 +0100

    Fügt Link auf BallSpiel Projekt ein
    siehe #3

--- WPF-1/WPF1-Unterfenster/WPF1-Unterfenster/SubWindow.xaml.cs
+++ WPF-1/WPF1-Unterfenster/WPF1-Unterfenster/SubWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,21 @@ namespace WPF1_Unterfenster
         public SubWindow()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            // Wenn ein Prozess mit einer URL gestartet wird, wird automatisch der default Browser geöffnet
+            var defaultBrowser = new ProcessStartInfo(e.Uri.AbsoluteUri)
+            {
+                UseShellExecute = true
+            };
+
+            // Der gestartet Prozess interessiert uns nicht weiter hier im Code. Daher verwenden wir _
+            _ = Process.Start(defaultBrowser);
+            
+            // Dieses Event ist nun ausreichend behandelt und braucht nicht mehr weiter gegeben werden.
+            e.Handled = true;
         }
     }
 }
