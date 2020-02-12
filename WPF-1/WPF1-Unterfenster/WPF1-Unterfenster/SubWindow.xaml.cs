e599837d9dce7bf92bceb04dd167adedd33f57f2
Student bei LernMoment <student@lernmoment.de>
2020-02-12 09:09:47 +0100

    Zeigt SubWindow als Fenster an
    siehe #3

--- WPF-1/WPF1-Unterfenster/WPF1-Unterfenster/SubWindow.xaml.cs
+++ WPF-1/WPF1-Unterfenster/WPF1-Unterfenster/SubWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPF1_Unterfenster
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für SubWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SubWindow : Window
     {
-        public MainWindow()
+        public SubWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            SubWindow sub = new SubWindow();
-            sub.Show();
         }
     }
 }
