7e2f5c6651dd9023e6132a2127ec13c09c08abb2
Student bei LernMoment <student@lernmoment.de>
2020-02-12 08:00:16 +0100

    Fügt Beispiele für ALL-1 ein

---
+++ All1-Community-Frage_Framework/All1-Community-Frage_Framework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All1_Community_Frage_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo LernMoment Community!");
+
+            Console.ReadLine();
+        }
+    }
+}
