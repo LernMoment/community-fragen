b00f557599ba1981062770498af56049422d6db4
Jan Suchotzki <jan@lernmoment.de>
2020-03-05 23:12:37 +0100

    Fügt Lösung für SNY-1 hinzu

---
+++ SYN-1/CommunityFragenSyn1/CommunityFragenSyn1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CommunityFragenSyn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte gebe einen Text ein: ");
+            string text = Console.ReadLine();
+            char[] sUD = new char[text.Length];
+            for (int x = 0; x < text.Length; x++)
+            {
+                sUD[x] = text[x];
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Der eingegebene Text hat: {sUD.Length} Zeichnen");
+            foreach (char zeichen in sUD)
+            {
+                Console.WriteLine(zeichen);
+            }
+        }
+    }
+}
